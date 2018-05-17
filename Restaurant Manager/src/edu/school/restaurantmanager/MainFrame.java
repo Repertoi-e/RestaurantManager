40a4d062157ab7860b0646f033051e1fe1f17c09
unknown <bojidarvidenov03@gmail.com>
2018-05-17 10:30:05 +0300

    Added sample table drawing

--- Restaurant Manager/src/edu/school/restaurantmanager/MainFrame.java
+++ Restaurant Manager/src/edu/school/restaurantmanager/MainFrame.java
@@ -3,9 +3,41 @@ package edu.school.restaurantmanager;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
+
+import org.omg.PortableServer.ID_ASSIGNMENT_POLICY_ID;
+
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+
+class Table {
+	private JButton layout;
+	private int id;
+	
+	public Table() {
+		layout = new JButton();
+	}
+	
+	public JButton getLayout() {
+		return layout;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void seId(int id) {
+		this.id = id;
+	}
+	
+}
 
 public class MainFrame extends JFrame {
 
@@ -13,8 +45,13 @@ public class MainFrame extends JFrame {
 
 	/**
 	 * Launch the application.
+	 * @throws UnsupportedLookAndFeelException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 * @throws ClassNotFoundException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -32,11 +69,65 @@ public class MainFrame extends JFrame {
 	 */
 	public MainFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 588, 377);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		final int X = 5;
+		final int Y = 5;
+		final int TABLE_SIZE = 80; 
+		
+		Table[] tables = {
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+				new Table(),
+		};
+		int currX;
+		int currY;
+		int f = 0;
+		int g = 0;
+		for (int i = 0; i < tables.length; i++) {
+			if(X+TABLE_SIZE*g > 540) {
+				g= 0;
+				f++;
+				continue;
+			}
+			tables[i].seId(i);
+			tables[i].getLayout().setBounds(X+TABLE_SIZE*g, Y+TABLE_SIZE*f, TABLE_SIZE, TABLE_SIZE);
+			tables[i].getLayout().setText("Table"+tables[i].getId());
+			contentPane.add(tables[i].getLayout());
+			g++;
+		}
+			
 	}
-
 }
