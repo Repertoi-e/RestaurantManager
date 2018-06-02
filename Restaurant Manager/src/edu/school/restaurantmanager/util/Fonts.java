f959a0aaffc23b69a8f604c4e10b4fa6e995d14b
Repertoi-e <dsotirov@windowslive.com>
2018-06-02 13:19:58 +0300

    Fix fonts

--- Restaurant Manager/src/edu/school/restaurantmanager/util/Fonts.java
+++ Restaurant Manager/src/edu/school/restaurantmanager/util/Fonts.java
@@ -1,5 +1,7 @@
 package edu.school.restaurantmanager.util;
 
+import edu.school.restaurantmanager.MainFrame;
+
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 
@@ -7,10 +9,10 @@ public class Fonts {
 	public static void registerFonts() {
 		try {
 			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-			ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, Fonts.class.getResourceAsStream("SourceSansPro-Regular.ttf")));
-			ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, Fonts.class.getResourceAsStream("SourceSansPro-Bold.ttf")));
-			ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, Fonts.class.getResourceAsStream("SourceSansPro-Italic.ttf")));
-			ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, Fonts.class.getResourceAsStream("SourceSansPro-BoldItalic.ttf")));
+			ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, MainFrame.class.getResourceAsStream("SourceSansPro-Regular.ttf")));
+			ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, MainFrame.class.getResourceAsStream("SourceSansPro-Bold.ttf")));
+			ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, MainFrame.class.getResourceAsStream("SourceSansPro-Italic.ttf")));
+			ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, MainFrame.class.getResourceAsStream("SourceSansPro-BoldItalic.ttf")));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
